44244a85a87766a5ff41918e029c3488a8f1d63f
muhand <muhandjumah@gmail.com>
2016-12-19 20:48:16 -0500

    Added the functionality to find the X and Y when someone touchs the screen

--- AutoClicker/app/src/main/java/com/muhandjumah/autoclicker/MainActivity.java
+++ AutoClicker/app/src/main/java/com/muhandjumah/autoclicker/MainActivity.java
@@ -2,12 +2,56 @@ package com.muhandjumah.autoclicker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Private Members
+    private boolean detectLocation = false;
+    private int currentX = -1;
+    private int currentY = -1;
+
+    //Private controls
+    private Button locationBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        locationBtn = (Button) findViewById(R.id.locationBtn);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(detectLocation) {
+            int x = (int) event.getX();
+            int y = (int) event.getY();
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                case MotionEvent.ACTION_MOVE:
+                case MotionEvent.ACTION_UP:
+                    currentX = x;
+                    currentY = y;
+                    Log.d("X", "x: " + currentX);
+                    Log.d("Y", "y: " + currentY);
+            }
+        }
+        return false;
+    }
+
+    public void enableLocationDetection(View view)
+    {
+        if(detectLocation) {
+            detectLocation = false;
+            locationBtn.setText("Get Location");
+        }
+        else {
+            detectLocation = true;
+            locationBtn.setText("Disable Location");
+        }
     }
 }
